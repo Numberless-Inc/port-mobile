3fd5ef142615b9ec1234478835a9a1b2795c026b
Shantanav Saurav <shantanav.saurav@mail.numberless.tech>
2025-09-04 11:29:36 +0400

    Linting

--- src/utils/Backup/index.ts
+++ src/utils/Backup/index.ts
@@ -26,7 +26,7 @@ import {
   getSafeAbsoluteURI,
 } from '@utils/Storage/StorageRNFS/sharedFileHandlers';
 import {wait} from '@utils/Time';
-import {DEFAULT_BACKUP_INTERVAL, BackupIntervalString} from '@utils/Time/interfaces';
+import {BackupIntervalString, DEFAULT_BACKUP_INTERVAL} from '@utils/Time/interfaces';
 
 import NativeCryptoModule from '@specs/NativeCryptoModule';
 const {GoogleSignInModule} = NativeModules;
